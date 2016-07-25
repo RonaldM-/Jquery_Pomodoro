40a2244747dd7ff094d326474175d3a5768bd0ef
RonaldM- <ronald.marcel.mr@gmail.com>
2016-07-25 17:26:01 +0200

    crash test

--- js/script.js
+++ js/script.js
@@ -13,7 +13,62 @@
 
 Vous avez un squelette de base contenant le code **HTML**, le code **CSS**, edt qui intègre **Bootstrap** et **jQuery**.*/
 
-$( "#play" ).click(function() {
+
+//      JAVASCRIPT
+
+    var set_minute = 25;
+    var set_second = 0;
+    var second_t = 60;
+    decrement_time();
+
+
+/*function addzero(i){
+    if(i<10){
+        //i  = string de 0+ i => "0"+1=>01
+        i="0"+i;
+    }
+    return i;
+}*/
+
+
+//
+
+function decrement_time(){
+    var k;
+
+    for(k = second_t; k>0; k--){
+        var second = k;
+
+
+        console.log(second);
+
+        console.log("valeur html: "+ time);
+        time = set_minute +":"+ second;
+
+        console.log("qu'elle est ta valeur: "+ time);
+        var Timer = setInterval(time, 1000);
+        };
+    var time = document.getElementById("timer").innerHTML;
+    document.getElementById("timer").innerHTML = time;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$( "#play" ).click(function() {
   alert( "Handler for .click() called." );
 });
 
@@ -22,5 +77,6 @@ $( "#stop" ).click(function(){
 });
 
 $( "#pause" ).click(function(){
-    
+
 });
+*/
