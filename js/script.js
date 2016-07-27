faf391c3c1f56b30d02bbbbc76ad0b86155f71dd
RonaldM- <ronald.marcel.mr@gmail.com>
2016-07-27 16:41:22 +0200

    fonction incrementation en test

--- js/script.js
+++ js/script.js
@@ -15,11 +15,68 @@ Vous avez un squelette de base contenant le code **HTML**, le code **CSS**, edt 
 
 
 //      JAVASCRIPT
-
+ var sec = 0;
+    var second_sup = 0;
     var set_minute = 25;
-    var set_second = 0;
-    var second_t = 60;
-    decrement_time();
+    var set_second = 00;
+    timer_show();
+
+function addzero(i){
+    if (i < 10){
+        i = "0"+i;
+    }
+    return i;
+};
+function timer_show(){
+
+    var html_time = document.getElementById("timer").innerHTML;
+        /*console.log("qu'elle est ta valeur dans l'html: "+ html_time);*/
+        /*console.log("set_second (avant modif) = " + set_second);*/
+        set_second = increment_second();
+        /*console.log("set_second (après modif) = " + set_second);*/
+        html_time = set_minute +":"+ addzero(set_second);
+        document.getElementById("timer").innerHTML = html_time;
+        /*console.log("qu'elle est ta valeur: "+ html_time); */
+};
+
+
+//TODO: [] Ajout une second en ++
+function increment_second(){
+    /*sec ++ ;
+    return sec;*/
+var second_minute;
+    second_minute = 60;
+//TODO [] Parcourir un tableau.
+    console.log(second_minute.length);
+
+    for(var i = 0; i < second_minute.length; i++){
+        var second_plus = second_minute[i];
+
+        console.log("Qu'est ce que celà donne: "+ second_plus);
+       // break;
+    }
+    return second_plus;
+}
+
+
+
+/*function auto_click(){
+
+ 	if(score >= cout_autoclick){
+ 		var auto_interval;
+
+ 		ratio_autoclick += 10;
+ 		auto_interval= setInterval(function(){ compteur(); }, 2000+ratio_autoclick);
+ 		score-=cout_autoclick;
+ 		incrementeAutoclick();
+
+ 		console.log("Autoclick à une valeur de:"+cout_autoclick);
+ 		console.log("Autoclick à un ratio de:"+ratio_autoclick);
+ 	}
+ 	else{
+ 		console.log("Autoclick à une valeur de:"+cout_autoclick);
+ 	}
+}*/
 
 
 /*function addzero(i){
@@ -31,27 +88,24 @@ Vous avez un squelette de base contenant le code **HTML**, le code **CSS**, edt 
 }*/
 
 
-//
-
-function decrement_time(){
-    var k;
-
-    for(k = second_t; k>0; k--){
-        var second = k;
 
 
+/*function decrement_time(){
+    for(var i; i = 0; i > second_t; i++){
+        var second = i;
         console.log(second);
-
         console.log("valeur html: "+ time);
-        time = set_minute +":"+ second;
 
+        time = set_minute +":"+ second;
         console.log("qu'elle est ta valeur: "+ time);
-        var Timer = setInterval(time, 1000);
+
+        //var Timer = setInterval(time, 1000);
         };
+
     var time = document.getElementById("timer").innerHTML;
     document.getElementById("timer").innerHTML = time;
 
-}
+}*/
 
 
 
@@ -73,7 +127,7 @@ function decrement_time(){
 });
 
 $( "#stop" ).click(function(){
-    
+
 });
 
 $( "#pause" ).click(function(){
